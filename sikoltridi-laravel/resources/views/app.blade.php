9dd62255f7e9c7c4742d053a90ddd54d9e63fc40
dimszky <schooldims@gmail.com>
2025-11-21 20:52:35 +0700

    up

--- sikoltridi-laravel/resources/views/app.blade.php
+++ sikoltridi-laravel/resources/views/app.blade.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sikoltridi</title>
+    
     @viteReactRefresh
-    @vite('resources/js/main.jsx')
+    
+    {{-- Ubah path ini agar sama persis dengan input di vite.config.js --}}
+    @vite('src/main.jsx')
 </head>
 <body>
     <div id="root"></div>
